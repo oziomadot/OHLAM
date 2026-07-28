e9c5487c5dc77cb442b9ae68d023a573c603a574
ozioma <christ.stephaug@gmail.com>
2026-07-28 08:32:05 +0200

    Fix Expo SDK 57 build and path aliases

--- babel.config.js
+++ babel.config.js
@@ -1,25 +1,7 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
+
   return {
     presets: ['babel-preset-expo'],
-    plugins: [
-      [
-        'module-resolver',
-        {
-          root: ['./'],
-          extensions: ['.js', '.jsx', '.ts', '.tsx', '.json'],
-          alias: {
-            '@/components': './src/components',
-            '@/hooks': './src/hooks',
-            '@/utils': './src/utils',
-            '@/config': './src/config',
-            '@/src': './src',
-            '@/assets': './assets',
-
-
-          },
-        },
-      ],
-    ],
   };
 };
